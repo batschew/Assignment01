a20242e6cfded513b826b4c0e8c4ece360f63b0e
Steve Meckstroth <meckstss@gmail.com>
2018-01-10 14:04:27 -0500

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * @author Stephen Meckstroth and Evan Batschew
+ * Assignment 01
+ * Course IT2045 Section 001
+ * Due Date: Jan 16, 2018
+ * Description: Create a an eclipse java project that contains a stub class to calculate compound interest
+ * Citations: N/A
+ * 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
